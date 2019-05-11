4e96607a7311b10f7f5e710948efb63af08b2e50
thvaisa <thvaisa@gmail.com>
2019-05-11 12:55:28 +0300

    laser fixes

--- Assets/Scripts/SelectionPanel.cs
+++ Assets/Scripts/SelectionPanel.cs
@@ -11,19 +11,31 @@ public class SelectionPanel : MonoBehaviour
     public Button minusButton;
 
     public int indx = 0;
+    
     List<string> names;
     
+    public void Start()
+    {
+        names = new List<string>();
+    }
+
+
     // Start is called before the first frame update
     public void SetList(List<string> nameList)
     {
-        names = nameList;
+        foreach (var name in nameList)
+        {
+            names.Add(name);
+        }
         plusButton.onClick.AddListener(Plus);
         minusButton.onClick.AddListener(Minus);
+        text.text = names[indx];
     }
 
 
     void Plus()
     {
+      
         ++indx;
         CheckLimits();
         UpdateText();
