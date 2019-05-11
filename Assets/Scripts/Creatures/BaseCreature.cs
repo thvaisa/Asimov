8274077d9a3be63446650ea3f3aa62768c5b7db2
Jeroen <jeroen.bosch@nonoa.nl>
2019-05-11 13:03:31 +0300

    hive
    hive

---
+++ Assets/Scripts/Creatures/BaseCreature.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BaseCreature : MonoBehaviour
+{
+    HiveBehaviour hive;
+
+    Vector2 startPosition;
+    Vector2 moveDestination;
+
+    protected virtual void Start()
+    {
+        hive = GameObject.FindObjectOfType<HiveBehaviour>();
+        hive.AddCreatureToHive(this);
+
+        startPosition = GetStartingPosition();
+    }
+
+
+    protected virtual void Update()
+    {
+        MoveAround();
+    }
+
+    protected virtual Vector3 GetStartingPosition ()
+    {
+        return transform.position;
+    }
+
+    protected virtual void MoveAround ()
+    {
+        Vector2 randomDirection = Random.insideUnitCircle * hive.roamRadius;
+        moveDestination = randomDirection + startPosition;
+
+        transform.position= Vector2.MoveTowards(transform.position, moveDestination, 1f);
+    }
+
+    protected virtual void HitByLaser ()
+    {
+
+    }
+
+    protected virtual void Die ()
+    {
+        hive.RemoveCreatureFromHive(this);
+    }
+
+
+}
---
+++ Assets/Scripts/Creatures/HiveBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HiveBehaviour : MonoBehaviour
+{
+    public List<BaseCreature> creatures;
+
+    public float roamRadius = 30f;
+    //public NavMesh _nav;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void AddCreatureToHive (BaseCreature newCreature)
+    {
+        creatures.Add(newCreature);
+    }
+
+    public void RemoveCreatureFromHive(BaseCreature removeCreature)
+    {
+        creatures.Remove(removeCreature);
+    }
+}
