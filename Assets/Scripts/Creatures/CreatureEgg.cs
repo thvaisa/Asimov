ea250e4e05a563562939c9aa1daa17858995450e
Jeroen <jeroen.bosch@nonoa.nl>
2019-05-11 16:46:59 +0300

    queen lays eggs

--- Assets/Scripts/Creatures/CreatureEgg.cs
+++ Assets/Scripts/Creatures/CreatureEgg.cs
@@ -2,17 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CreatureEgg : MonoBehaviour
+public class CreatureEgg : BaseCreature
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject soldierPrefab;
+    public GameObject workerPrefab;
+    public GameObject queenPrefab;
+
+    float hatchTimer = 0f;
+    float hatchTimeLimit = 30f; //in seconds
+
+    protected override void Start()
     {
-        
+        base.Start();
+        hatchTimeLimit = hive.eggHatchTime;
     }
 
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
-        
+        //base.Update(); //no moving around for eggs
+        CheckForHatching();
+    }
+
+    void CheckForHatching ()
+    {
+        if (hatchTimer > hatchTimeLimit)
+        {
+            Hatch();
+        } else
+        {
+            hatchTimer += Time.deltaTime;
+        }
+    }
+
+    void Hatch ()
+    {
+        float random = Random.Range(0, 100f);
+
+        GameObject newCreature;
+        if (random < 50)
+        {
+            newCreature = Instantiate(soldierPrefab);
+        } else
+        {
+            newCreature = Instantiate(workerPrefab);
+        }
+        newCreature.transform.position = transform.position;
+        newCreature.transform.SetParent(hive.transform);
+
+        //Play sound here?
+        Destroy(this.gameObject);
     }
 }
